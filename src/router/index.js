42ec2a1b658c0198e441a6770814717a6579e432
jchacko11 <jonathan.j.chacko@gmail.com>
2020-05-23 22:36:10 -0500

    Final changes on this branch

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../components/Login.vue'
-import particles from '../components/Particles.vue'
 
 Vue.use(VueRouter)
 
@@ -10,21 +9,25 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Lesson - Learning Reimagined',
+    }
   },
     {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+      meta: {
+        title: 'Login - Learning Reimagined',
+      }
   },
-    {
-      path: '/particles',
-      name: 'particles',
-      component: particles
-    },
   {
     path: '/about',
     name: 'About',
+    meta: {
+      title: 'About - Learning Reimagined',
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -32,10 +35,18 @@ Vue.use(VueRouter)
   }
 ]
 
+
+
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
 
 export default router
